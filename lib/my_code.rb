f21eb163eb63afbf61ad3021e022448bb526fc66
Wesley Chen <weschen1996@gmail.com>
2020-01-19 01:27:55 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,29 @@
 # Your Code Here
+def map(arr)
+  ans = []
+  count = 0
+  while count < arr.length
+    ans.push(yield(arr[count]))
+    count += 1
+  end
+  return ans
+end
+
+def reduce(arr, block=nil)
+
+  
+  if block != nil
+    ans = block
+    count = 0
+  else
+    ans = arr[0]
+    count = 1
+  end
+  
+  while count < arr.length 
+    ans = yield(ans, arr[count])
+    count += 1
+  end
+  
+  return ans
+end
